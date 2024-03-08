5fb6cbf2303a2613e903482e93974368693ce5bf
DUY PHAM <d.pham1801@tcu.edu>
2024-03-08 11:15:59 -0600

    testing

---
+++ src/main/java/edu/tcu/hogwartsartifactsonline/wizard/Wizard.java
@@ -0,0 +1,32 @@
+package edu.tcu.hogwartsartifactsonline.wizard;
+
+import java.io.Serializable;
+
+public class Wizard implements Serializable {
+
+    private Integer id;
+
+    private String name;
+
+    public Wizard(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
